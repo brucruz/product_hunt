61332a40bb6f4bd6d325da1c4fd52aa4ccae7f53
Bruno Cruz <bru.cruz@gmail.com>
2020-03-24 20:47:10 -0300

    criado model de produto com exemplo para a heali

--- node-api/server.js
+++ node-api/server.js
@@ -1,18 +1,31 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const requireDir = require('require-dir');
 
 // Iniciando o App
 const app = express();
 
 //Iniciando o DB
-mongoose.connect('mongodb://localhost:27017/nodeapi', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
+mongoose.connect(
+    'mongodb://localhost:27017/nodeapi', 
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+);
+requireDir('./src/models');
+
+const Product = mongoose.model('Product');
 
 // Primeira rota
 app.get('/', (req, res) => {
-    res.send('Hello Heali');
+    Product.create({
+        title: 'Heali',
+        description: 'Buscador de preços de exames médicos',
+        url: 'https://heali.me'
+    });
+    
+    return res.send('Hello Heali');
 });
 
 app.listen(3001);
